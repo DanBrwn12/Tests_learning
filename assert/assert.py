84ee89ae9d62f66ce91ef96acca48d8c0db3ecb8
Vladimir <daway4554@gmail.com>
2022-06-05 18:10:56 +0300

    добавил для теста возможные варитны ввода пароля

---
+++ assert/assert.py
@@ -0,0 +1,28 @@
+# Написать функцию провреки "Силы" пароля, возвращает всегда строку
+# - если пароль короче 8 символов то вернуть Too Weak
+# - если пароль содержит только цифры , только строчные, только заглавные то вернуть Weak
+# - если пароль содержит символы любых 2 наборов вернуть Good
+# - если пароль содержит символы любых 3 наборов вернуть Very Good
+
+def password_strength(value:str)->str:
+    pass
+
+if __name__ == '__main__':
+    assert password_strength('')=='Too Weak'
+    assert password_strength('1234567')=='Too Weak'
+    assert password_strength('asdewe')=='Too Weak'
+    assert password_strength('QAZXDDF')=='Too Weak'
+    assert password_strength('QAzX1D5')=='Too Weak'
+    assert password_strength('12345678')=='Weak'
+    assert password_strength('1333332345678')=='Weak'
+    assert password_strength('asdfghjk')=='Weak'
+    assert password_strength('asdasdgdfghjk')=='Weak'
+    assert password_strength('ASDFGHJK')=='Weak'
+    assert password_strength('ASdF1HJ5')=='Weak'
+    assert password_strength('1234asdf')=='Good'
+    assert password_strength('1234ASDF')=='Good'
+    assert password_strength('asdfASDF')=='Good'
+    assert password_strength('asdfdasdfASDF')=='Good'
+    assert password_strength('123asdDG')=='Very Good'
+    assert password_strength('123asddsadDG')=='Very Good'
+    assert password_strength('123asddsadDG')=='Very Good'
