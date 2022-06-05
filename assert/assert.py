5208c516719dca20145cb5bef207dd99138069e9
Vladimir <daway4554@gmail.com>
2022-06-05 19:37:37 +0300

    Добавил условие для очень хорошего пароля Убрал ошибочную строку с тестом

--- assert/assert.py
+++ assert/assert.py
@@ -13,12 +13,16 @@ def password_strength(value: str) -> str:
     uppers = lowers.upper()
     if len(value) < 8:
         return 'Too Weak'
-# если все буквы в value содержаться в digits или lowers или uppers
+    # если все буквы в value содержат digits или lowers или uppers
     if all(e in digits for e in value) or all(e in lowers for e in value) or all(e in uppers for e in value):
         return 'Weak'
-    if (any(e in digits for e in value) and any(e in lowers for e in value)) or (any(e in digits for e in value) and any(e in uppers for e in value)) or (any(e in lowers for e in value) and any(e in uppers for e in value)):
+    if any(e in digits for e in value) and any(e in lowers for e in value) and any(e in uppers for e in value):
+        return 'Very Good'
+    if (any(e in digits for e in value) and any(e in lowers for e in value)) or (
+            any(e in digits for e in value) and any(e in uppers for e in value)) or (
+            any(e in lowers for e in value) and any(e in uppers for e in value)):
         return 'Good'
-    return ''
+
 
 
 if __name__ == '__main__':
@@ -32,11 +36,10 @@ if __name__ == '__main__':
     assert password_strength('asdfghjk') == 'Weak'
     assert password_strength('asdasdgdfghjk') == 'Weak'
     assert password_strength('ASDFGHJK') == 'Weak'
-    assert password_strength('ASdF1HJ5') == 'Weak'
     assert password_strength('1234asdf') == 'Good'
     assert password_strength('1234ASDF') == 'Good'
     assert password_strength('asdfASDF') == 'Good'
     assert password_strength('asdfdasdfASDF') == 'Good'
-    assert password_strength('123asdDG') == 'Very Good'
-    assert password_strength('123asddsadDG') == 'Very Good'
-    assert password_strength('123asddsadDG') == 'Very Good'
+    assert password_strength('12asdDGN') == 'Very Good'
+    assert password_strength('DBNMasddsad321') == 'Very Good'
+    assert password_strength('123NRJDKdngjd') == 'Very Good'
